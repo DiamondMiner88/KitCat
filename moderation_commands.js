2fb824afe1906eaa6a382ba2cb902a678e380059
Diamond <DiamondMiner88@users.noreply.github.com>
2020-06-11 15:40:34 -0700

    silent ban

--- moderation_commands.js
+++ moderation_commands.js
@@ -66,19 +66,63 @@ function moderation_commands(message, command, args, client) {
         });
       }
       if (channel !== undefined) channel.send(`\`${message.author.tag}\` has banned \`${target_member.tag}\`.`);
-      else console.log(`welcome channel is missing`);
+      else console.log(`bot-log channel is missing`);
     }
     else {
       message.channel.send(`You do not the permission to ban members.`)
     }
   }
-  if (command === "kick") {
+  else if (command === "sban") {
+    if (args[0] === undefined) {
+      message.author.send(`You need to mention someone or put their tag.`);
+      return;
+    }
+    let target_user = message.mentions.users.first();
+    if (!target_user) {
+      let matching_users = client.users.cache.filter(user => user.username === args[0].split("#")[0]);
+      target_user = matching_users.find(user => user.discriminator === args[0].split("#")[1]);
+      if (!target_user) {
+        message.channel.send('Member not found');
+        return;
+      }
+    }
+    if (message.member.hasPermission("BAN_MEMBERS")) {
+      let target_member = message.guild.members.cache.find(member => member.id === target_user.id);
+      channel = message.guild.channels.cache.find(channel => channel.name === 'bot-log');
+
+      args.shift();
+      let banLength;
+      if (args.length === 0) {
+        target_member.ban();
+      }
+      else if (isNaN(args[0])) {
+        message.author.send(`\`${args[1]}\` is not a number. Use 0 for a permenant ban.`);
+        return;
+      }
+      banLength = parseInt(args[0]);
+      args.shift()
+      if (args.length === 0) {
+        target_member.ban({
+          days: banLength
+        });
+      }
+      else {
+        target_member.ban({
+          days: banLength,
+          reason: args.join(" ")
+        });
+      }
+      if (channel !== undefined) message.author.send(`You have banned \`${target_member.tag}\`.`);
+      else console.log(`bot-log channel is missing`);
+    }
+    else {
+      message.author.send(`You do not the permission to ban members.`);
+    }
+  }
+  else if (command === "skick") {
 
   }
-  if (command === "sban") {
-
-  }
-  if (command === "skick") {
+  else if (command === "kick") {
 
   }
 }
