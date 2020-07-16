ed8a242e824219280b1ffd26e00770ec3d5b0765
Nano-AI <adi.bankoti@gmail.com>
2020-07-15 21:14:26 -0700

    Added more code to Trivia.js

--- commands/trivia.js
+++ commands/trivia.js
@@ -5,7 +5,6 @@ const fetch = require('node-fetch');
 
 // https://opentdb.com/api_config.php
 const categories = {
-<<<<<<< HEAD
     'generalknowledge':'any',
     'entertainment:books':9,
     'entertainment:film':10,
@@ -51,14 +50,14 @@ const triviaHelp = new Discord.MessageEmbed()
     .addField('Categories', '`General Knowledge`, `Entertainment: Books`, `Entertainment: Film`, `Entertainment: Music`, `Entertainment: Musicals & Theatres`, `Entertainment: Television`, `Entertainment: Video Games`, `Entertainment: Board Games`, `Science &amp; Nature`, `Science: Computers`, `Science: Mathematics`, `Mythology`, `Sports`, `Geography`, `History`, `Politics`, `Art`, `Celebrities`, `Animals`, `Vehicles`, `Entertainment: Comics`, `Science: Gadgets`, `Entertainment: Japanese Anime & Manga`, `Entertainment: Cartoon & Animations`')
     .addField('Difficulties', '`Any Difficulty`, `Easy`, `Medium`, `Hard`')
     .addField('Type of Questions', '`Any Type`, `Multiple Choice`, `True False`')
-    .addField('Ussage', `\`\`${pfx}trivia \`{amount}\` \`{category}\` \`{difficulty}\` \`{type}\` \`\``)
+    .addField('Ussage', `\`\`${pfx}trivia \`{category}\` \`{difficulty}\` \`{type}\` \`\``)
 ;
 
 module.exports = {
     command: "trivia",
     category: "fun",
     help_name: `:question: Trivia`, 
-    help_description: `Asks a trivia question!\n\`\`${pfx}trivia \`{amount}\` \`{category}\` \`{difficulty}\` \`{type}\` \`\`.\nRun \`${pfx}trivia help\` for help with the trivia command.`, 
+    help_description: `Asks a trivia question!\n\`\`${pfx}trivia \`{category}\` \`{difficulty}\` \`{type}\` \`\`.\nRun \`${pfx}trivia help\` for help with the trivia command.`, 
     execute(client, message, args) {
         if (args[0] === "help") {      
             return message.channel.send(triviaHelp);
@@ -67,83 +66,21 @@ module.exports = {
             return /\S/.test(str);
         });
         // if (input.length < 3) return message.channel.send("You didn't provide valid paramaters.");
-        var amount = parseInt(input[0]);
-        if (amount === NaN) return message.channel.send("You provided number count!");
         // console.log(input[1].toLowerCase().replace(/\s+/g, ''));
-        var category = categories[input[1].toLowerCase().replace(/\s+/g, '')];
-        if (category === undefined) return message.channel.send("You provided invalid category!");
-        var difficulty = input[2];
-        if (difficulty.toLowerCase().replace(/\s+/g, '') != "anydifficulty" && difficulty.toLowerCase().replace(/\s+/g, '') != 'easy' && difficulty.toLowerCase().replace(/\s+/g, '') != 'medium' && difficulty.toLowerCase().replace(/\s+/g, '') != 'hard') {
-            return message.channel.send("You provided an invalid difficulty!");
-        }
-        var toq = input[3];
-        if (toq.toLowerCase().replace(/\s+/g, '') != 'multiplechoice' && toq.toLowerCase().replace(/\s+/g, '') != 'truefalse' && toq.toLowerCase().replace(/\s+/g, '') != 'anytype') {
-            return message.channel.send("You provided an invalid type for the questions");
-        }
-        toq = selectedType[toq];
-        difficulty = selectedDifficulty[difficulty];
-=======
-  'General Knowledge': 'any',
-  'Entertainment: Books': 9,
-  'Entertainment: Film': 10,
-  'Entertainment: Music': 11,
-  'Entertainment: Musicals & Theatres': 12,
-  'Entertainment: Television': 13,
-  'Entertainment: Video Games': 14,
-  'Entertainment: Board Games': 15,
-  'Science &amp; Nature': 16,
-  'Science: Computers': 17,
-  'Science: Mathematics': 18,
-  'Mythology': 19,
-  'Sports': 20,
-  'Geography': 21,
-  'History': 22,
-  'Politics': 23,
-  'Art': 24,
-  'Celebrities': 25,
-  'Animals': 26,
-  'Vehicles': 27,
-  'Entertainment: Comics': 28,
-  'Science: Gadgets': 29,
-  'Entertainment: Japanese Anime & Manga': 30,
-  'Entertainment: Cartoon & Animations': 31
+        var category = categories[input[0].toLowerCase().replace(/\s+/g, '')];
+		var difficulty = selectedDifficulty[input[1].toLowerCase().replace(/\s+/g, '')];
+		var toq = selectedType[input[2].toLowerCase().replace(/\s+/g, '')];
+
+		if (category === undefined) return message.channel.send("You provided an invalid category!");
+		if (difficulty === undefined) return message.channel.send("You provided an invalid difficulty!");
+		if (toq === undefined) return message.channel.send("You provided an invalid type of question!")
+
+		var url = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${difficulty}&type=${toq}`;
+
+		console.log(url);
+	}
 }
-
-const triviaHelp = new Discord.MessageEmbed()
-  .setColor('#00008B')
-  .setTitle('Trivia Help')
-  .addField('Categories', '`General Knowledge`, `Entertainment: Books`, `Entertainment: Film`, `Entertainment: Music`, `Entertainment: Musicals & Theatres`, `Entertainment: Television`, `Entertainment: Video Games`, `Entertainment: Board Games`, `Science &amp; Nature`, `Science: Computers`, `Science: Mathematics`, `Mythology`, `Sports`, `Geography`, `History`, `Politics`, `Art`, `Celebrities`, `Animals`, `Vehicles`, `Entertainment: Comics`, `Science: Gadgets`, `Entertainment: Japanese Anime & Manga`, `Entertainment: Cartoon & Animations`')
-  .addField('Difficulties', '`Any Difficulty`, `Easy`, `Medium`, `Hard`')
-  .addField('Type of Questions', '`Multiple Choice`, `True False`')
-  .addField('Ussage', `\`\`${pfx}trivia \`{amount}\` \`{category}\` \`{difficulty}\` \`{type}\` \`\``);
-
-module.exports = {
-  command: "trivia",
-  category: "fun",
-  help_name: `:question: Trivia`,
-  help_description: `Asks a trivia question!\n\`\`${pfx}trivia \`{amount}\` \`{category}\` \`{difficulty}\` \`{type}\` \`\`.\nRun \`${pfx}trivia help\` for help with the trivia command.`,
-
-  execute(client, message, args) {
-    var string = "";
-    for (var i in categories) {
-      string += '`' + categories[i] + '`,'
->>>>>>> 0081d8ceee5df3ec0ddfa340b4f1bb959ae9e324
-    }
-    console.log(string);
-    if (args[0] === "help") {
-      return message.channel.send(triviaHelp);
-    }
-    var input = args.join(' ').split('`').filter(function(str) {
-      return /\S/.test(str);
-    });
-    // if (input.length < 3) return message.channel.send("You didn't provide valid paramaters.");
-    var amount = parseInt(input[0]);
-    if (amount === NaN) return message.channel.send("You provided invalid paramaters!");
-    var category = categories[input[1].toLowerCase().trim()];
-    if (category === undefined) return message.channel.send("You provided invalid category!!");
-  }
-}
-
+// You changed some crap here
 /*
  General Knowledge
  Entertainment: Books
