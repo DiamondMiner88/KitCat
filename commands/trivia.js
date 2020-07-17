15a8f7325665cc698e4bcfb28579f81fd46bddfc
Nano-AI <adi.bankoti@gmail.com>
2020-07-17 12:16:35 -0700

    Added coin system to true false questions

--- commands/trivia.js
+++ commands/trivia.js
@@ -69,7 +69,7 @@ const triviaHelp = new Discord.MessageEmbed()
   .addField('Categories', '`Any`, `General Knowledge`, `Entertainment: Books`, `Entertainment: Film`, `Entertainment: Music`, `Entertainment: Musicals & Theatres`, `Entertainment: Television`, `Entertainment: Video Games`, `Entertainment: Board Games`, `Science &amp; Nature`, `Science: Computers`, `Science: Mathematics`, `Mythology`, `Sports`, `Geography`, `History`, `Politics`, `Art`, `Celebrities`, `Animals`, `Vehicles`, `Entertainment: Comics`, `Science: Gadgets`, `Entertainment: Japanese Anime & Manga`, `Entertainment: Cartoon & Animations`')
   .addField('Difficulties', '`Any`, `Easy`, `Medium`, `Hard`')
   .addField('Type of Questions', '`Any`, `Multiple Choice`, `True False`')
-	.addField('Earn Money', '`Yes`, `No` (No by default)')
+  // .addField('Earn Money', '`Yes`, `No` (No by default)')
   .addField('Ussage', `\`\`${pfx}trivia \`{category}\` \`{difficulty}\` \`{type}\` \`\` or run \`${pfx}trivia\` for any category, any difficulty, and any type of question.`);;
 
 const boolQuestionFilterArray = [
@@ -88,7 +88,7 @@ module.exports = {
   command: "trivia",
   category: cmdCategories.fun,
   help_name: `:question: Trivia`,
-  help_description: `Asks a trivia question!\n\`\`${pfx}trivia \`{category}\` \`{difficulty}\` \`{type}\` \`{earn money}\`.\nRun \`${pfx}trivia help\` for help with the trivia command.`,
+  help_description: `Asks a trivia question!\n\`\`${pfx}trivia \`{category}\` \`{difficulty}\` \`{type}\`. If you get the question right, you earn oof coins, if you get it wrong, you loose oof coins.\nRun \`${pfx}trivia help\` for help with the trivia command.`,
   // Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch is located on which Welsh island?
   execute(client, message, args) {
     // console.log(decode(`Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch is located on which Welsh island?`))
@@ -100,9 +100,10 @@ module.exports = {
     });
     var url = `https://opentdb.com/api.php?amount=1`;
     var user = message.author;
+    var difficulty;
     if (args.length != 0) {
       var category = categories[input[0].toLowerCase().replace(/\s+/g, '')];
-      var difficulty = selectedDifficulty[input[1].toLowerCase().replace(/\s+/g, '')];
+      difficulty = selectedDifficulty[input[1].toLowerCase().replace(/\s+/g, '')];
       var toq = selectedType[input[2].toLowerCase().replace(/\s+/g, '')];
 
       if (category === undefined) return message.channel.send("You provided an invalid category!");
@@ -112,11 +113,16 @@ module.exports = {
       if (difficulty != "any") url += `&difficulty=${difficulty}`;
       if (toq != "any") url += `&type=${toq}`;
     }
+    console.log(difficulty);
     fetch(url, {
         method: 'Get'
       })
       .then(res => res.json())
       .then((json) => {
+        if (difficulty === undefined) {
+          var difficulty = json.results[0].difficulty.charAt(0).toUpperCase() + json.results[0].difficulty.slice(1);
+        }
+				console.log("difficulty: " + difficulty);
         var arrayAnswers = json.results[0].incorrect_answers;
         arrayAnswers.push(json.results[0].correct_answer);
         arrayAnswers = shuffle(arrayAnswers);
@@ -182,67 +188,62 @@ module.exports = {
               }).then(collected => {
                 collected.each(reaction => {
                   if (reaction._emoji.name === multipleQuestionFilterArray[answers.answers.indexOf(answers.correct_answer)]) {
-										var earned = 0;
-										switch(difficulty){
-											case 'Easy':
-												earned = 1
-												break;
-											case 'Medium':
-												earned = 2;
-												break;
-											case 'Hard':
-												earned = 4;
-												break;
-										}
-										db.run("UPDATE currency SET purse = purse + ? WHERE user = ?", [earned, user.id], (err) => {
-											console.error(err);
-										});
-										db.get("SELECT purse, bank, loan_left, loan_due FROM currency WHERE user=?", [user.id], (err, result) => {
-											message.channel.send(`<@${user.id}>, correct! The answer to \`${answers.question}\` is \`${answers.correct_answer}\`.` +
-											`You earned ${earned} oofcoins.`);
-								    });
-										return;
-                  }
-
-									else {
-										var lost = 0;
-										switch(difficulty){
-											case 'Easy':
-												break;
-											case 'Medium':
-												lost = 1;
-												break;
-											case 'Hard':
-												lost = 3;
-												break;
-										}
-										db.run("UPDATE currency SET purse = purse - ? WHERE user = ?", [lost, user.id], (err) => {
-											console.error(err);
-										});
-										db.get("SELECT purse, bank, loan_left, loan_due FROM currency WHERE user=?", [user.id], (err, result) => {
-											message.channel.send(`<@${user.id}>, correct! The answer to \`${answers.question}\` is \`${answers.correct_answer}\`.` +
-											`You lost ${lost} oofcoins.`);
-								    });
+                    var earned = 0;
+                    switch (difficulty) {
+                      case 'Easy':
+                        earned = 1
+                        break;
+                      case 'Medium':
+                        earned = 2;
+                        break;
+                      case 'Hard':
+                        earned = 4;
+                        break;
+                    }
+                    db.run("UPDATE currency SET purse = purse + ? WHERE user = ?", [earned, user.id], (err) => {
+                      if (err) console.log(err);
+                    });
+                    message.channel.send(`<@${user.id}>, correct! The answer to \`${answers.question}\` is \`${answers.correct_answer}\`.` +
+                      `You earned ${earned} oofcoins.`);
                     return;
-									}
+                  }
+                  else {
+                    var lost = 0;
+                    switch (difficulty) {
+                      case 'Easy':
+											break;
+                      case 'Medium':
+                        lost = 1;
+                        break;
+                      case 'Hard':
+                        lost = 3;
+                        break;
+                    }
+                    db.run("UPDATE currency SET purse = purse - ? WHERE user = ?", [lost, user.id], (err) => {
+                      if (err) console.log(err);
+                    });
+                    message.channel.send(`<@${user.id}>, incorrect! The answer to \`${answers.question}\` is \`${answers.correct_answer}\`.` +
+                      `You lost ${lost} oofcoins.`);
+                    return;
+                  }
                 });
               }).catch(err => {
-								var lost = 0;
-								switch(difficulty){
-									case 'Easy':
-										break;
-									case 'Medium':
-										lost = 1;
-										break;
-									case 'Hard':
-										lost = 3;
-										break;
-								}
-								db.run("UPDATE currency SET purse = purse - ? WHERE user = ?", [lost, user.id], (err) => {
-									console.error(err);
-								});
+                var lost = 0;
+                switch (difficulty) {
+                  case 'Easy':
+                    break;
+                  case 'Medium':
+                    lost = 1;
+                    break;
+                  case 'Hard':
+                    lost = 3;
+                    break;
+                }
+                db.run("UPDATE currency SET purse = purse - ? WHERE user = ?", [lost, user.id], (err) => {
+                  console.error(err);
+                });
                 return message.channel.send(`<@${user.id}>, you ran out of time! The answer to \`${answers.question}\` is \`${answers.correct_answer}\`.` +
-								`You lost ${lost} oofcoins.`);
+                  `You lost ${lost} oofcoins.`);
               });
             }).catch(function() {
               console.error("error");
