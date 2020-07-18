25a90e823e2f876eb63ecbd8330b0d1479e80d5f
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2020-07-18 13:00:16 -0700

    Update _SAMPLE_COMMAND.js

--- commands/_SAMPLE_COMMAND.js
+++ commands/_SAMPLE_COMMAND.js
@@ -1,1 +1,14 @@
 
+const config = require("../config.json");
+const pfx = config.prefix;
+const categories = require("./_CATEGORIES.js");
+
+module.exports = {
+  command: "COMMANDNAME",
+  category: categories.utils,
+  help_name: `help name`,
+  help_description: `help description`,
+
+  execute(client, message, args) {
+  }
+}
