c7738d65019356c1eeb2a4165bc4c302f8f8e98a
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2020-07-20 08:23:25 -0700

    restart/update commands

---
+++ run.js
@@ -0,0 +1,16 @@
+const { spawn } = require('child_process');
+
+var bot;
+
+function startBot() {
+  bot = spawn('node', ["index.js"]);
+
+  bot.stdout.on('data', data => console.log(data.toString()));
+  bot.stderr.on('data', data => console.error(data.toString()));
+
+  bot.on('close', code => {
+    if (code === 5001) startBot();
+  });
+}
+
+startBot();
