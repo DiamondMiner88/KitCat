c0baa89ad1e984fdcd4c73138f4da6703b4d21db
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2020-08-21 10:51:13 -0700

    Rewrite mcuser command

--- kitcat/commands/namemc.js
+++ kitcat/commands/namemc.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
 
 module.exports = {
   command: 'namemc',
@@ -16,22 +19,47 @@ module.exports = {
    * @param {Array.<String>} args
    */
   execute(message, args) {
-    if (args.length === 0) {
-      return message.channel.send("You didn't provide a Minecraft username.");
-    }
-    fetch(`https://minotar.net/armor/body/${args[0]}/100.png`, { method: 'HEAD' }).then((res) => {
-      if (res.ok) {
-        return message.channel.send(
-          new Discord.MessageEmbed()
-            .setTitle(`User "${args[0]}"`)
-            .setColor('#5E9D34')
-            .setImage(`https://minotar.net/armor/body/${args[0]}/100.png`)
-            .setDescription(
-              `[Download Skin](https://minotar.net/download/${args[0]})\n[NameMC](https://namemc.com/profile/${args[0]})`
-            )
-        );
-      }
-      return message.channel.send(`Invalid user: ${args[0]}`);
-    });
+    if (!args[0]) return message.channel.send("You didn't provide a username.");
+    fetch(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`)
+      .then((res) => res.json())
+      .then((user) => {
+        fetch(`https://api.mojang.com/user/profiles/${user.id}/names`)
+          .then((res) => res.json())
+          .then((names) => {
+            let embed = new Discord.MessageEmbed()
+              .setTitle(user.name)
+              .setDescription('Usernames:')
+              .setColor(0x00aa00)
+              .setURL(`https://namemc.com/profile/${user.name}`)
+              .setImage(`https://minotar.net/armor/body/${user.name}/100.png`);
+
+            if (names.error)
+              message.channel.send(
+                names.errorMessage +
+                  '\nPlease try again in a few seconds. I have no idea why this happens. Will try to fix later.'
+              );
+            else {
+              for (const name of names) {
+                const date = dayjs(name.changedToAt).utc().format('MMM D, YYYY h:mm:ss A');
+                embed.addField(
+                  name.name.replace('_', '\\_'),
+                  name.changedToAt ? (date + ' UTC').replace('_', '\\_') : 'Initial name'
+                );
+              }
+              message.channel.send(embed);
+            }
+          })
+          .catch((error) => {
+            message.channel.send(error.message);
+          });
+      })
+      .catch((error) => {
+        if (error.message.startsWith('invalid json response body at'))
+          message.channel.send('This this not a valid user!');
+        else {
+          console.error(error.message);
+          message.channel.send(error.message);
+        }
+      });
   }
 };
