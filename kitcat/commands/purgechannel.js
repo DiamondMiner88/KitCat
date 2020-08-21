fb5726568a380176e1e3f965308729d44aa05442
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2020-08-21 08:44:24 -0700

    Fix purgechannel

--- kitcat/commands/purgechannel.js
+++ kitcat/commands/purgechannel.js
@@ -7,25 +7,25 @@ module.exports = {
   help_description: `Used to delete to wipe all messages in a channel. ***This command deletes and makes a new channel in the same***`,
   usage: `purgechannel`,
   guildOnly: true,
-  unlisted: true,
+  unlisted: false,
 
+  /**
+   * Deletes the channel and creates a new one with the same properties
+   * @param {Discord.TextChannel} message
+   * @param {Array.<String>} args
+   */
   execute(message, args) {
-    return;
-    if (!message.member.hasPermission('MANAGE_MESSAGES')) {
-      message.channel.send('You do not have the permission to manage messages.');
-      return;
-    }
-    message.channel.send('This command is not implemented yet!');
+    if (!message.member.hasPermission('MANAGE_MESSAGES'))
+      return message.channel.send('You do not have the permission to manage messages.');
+    if (!message.channel.deletable)
+      return message.channel.send("I don't have permission to delete this channel!");
+
     const position = message.channel.rawPosition;
-    message.guild.channels.create(message.channel.name, { reason: `${message.author} purged the channel.` })
-      .then((item) => {
-        item.overwritePermissions(message.channel.permissionOverwrites);
-        message.channel.delete().then().catch(err => {console.log(err)});
-        item.setParent(message.channel.parent);
-        item.setPosition(position)
-          .then()
-          .catch(err => console.log(err));
-      })
-      .catch((err) => console.log(err))
+
+    message.channel.clone().then((channel) => {
+      message.channel.delete().then(() => {
+        channel.setPosition(position);
+      });
+    });
   }
 };
