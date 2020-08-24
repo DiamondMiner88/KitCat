c1157720fa023377191350b0f8cc01b44604f84a
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2020-08-24 12:55:11 -0700

    website env cookie

--- kitcat-react/src/functions.js
+++ kitcat-react/src/functions.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Cookies from 'universal-cookie';
 
 export function ReactIsInDevelomentMode() {
-  return '_self' in React.createElement('div');
+  return '_self' in React.createElement('div') && new Cookies().get('env') === 'development';
 }
