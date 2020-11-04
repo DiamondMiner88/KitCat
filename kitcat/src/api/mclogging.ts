171cd9e7a9c0517041b01b845c8f3449d3efdc55
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2020-11-04 10:40:37 -0800

    Mclogging missing access bug

--- kitcat/src/api/mclogging.ts
+++ kitcat/src/api/mclogging.ts
@@ -1,12 +1,19 @@
-import { TextChannel } from 'discord.js';
+import { Channel, TextChannel } from 'discord.js';
 import { Request, Response } from 'express';
 import { bot } from '../bot';
 
 export default async function (request: Request, response: Response) {
   const { username, action, channel } = request.body;
-  if (!username || !action) return response.status(400);
+  if (!username || !action) return response.status(400).send('');
 
-  const chnl = await bot.channels.fetch(channel);
-  if (!channel) return response.status(404);
+  let chnl: Channel;
+  try {
+    chnl = await bot.channels.fetch(channel);
+    if (!channel) throw Error;
+  } catch (error) {
+    return response.status(400).send('');
+  }
+
   (chnl as TextChannel).send(`${username} has ${action}.`);
+  response.send('');
 }
