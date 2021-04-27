f826f6b010631628cb4b778faf545f3b86f99cf1
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-04-27 13:06:33 -0700

    feat: steal command

--- packages/bot/src/modules/steal.ts
+++ packages/bot/src/modules/steal.ts
@@ -1,45 +1,107 @@
-import { Collection, CommandInteraction, Permissions } from 'discord.js';
-import { Module, ModuleCategory } from '../modules';
+import { CommandInteraction, DiscordAPIError, Permissions } from 'discord.js';
+import { Module, ModuleCategory, OptionRole, OptionString } from '../modules';
+import sharp from 'sharp';
+import fetch, { FetchError } from 'node-fetch';
 
 export default class extends Module {
   name = 'steal';
-  description = 'Create an emoji using an existing image/emoji.';
+  description = 'Clone an emoji or create from a url.';
   category = ModuleCategory.UTILITY;
   guildOnly = true;
   advancedPermissions = true;
   userPermissions = [Permissions.FLAGS.MANAGE_EMOJIS];
-  unlisted = true;
 
   options: any = [
     {
       type: 'STRING',
+      name: 'name',
+      description: "Create from url. **Will compress image if it's bigger than 256kb**",
+      required: true,
+      choices: undefined,
+      options: undefined
+    },
+    {
+      type: 'STRING',
       name: 'emoji',
-      description: "Use an existing emoji from another server or give it's id",
+      description: 'Use an existing emoji or an emoji id.',
       required: false,
-      default: undefined,
       choices: undefined,
       options: undefined
     },
     {
       type: 'STRING',
       name: 'url',
-      description: "Create from url. **Will compress image if it's bigger than 256kb**",
+      description: 'Create from url. Will downscale to 128x128 if too big.',
       required: false,
-      default: undefined,
+      choices: undefined,
+      options: undefined
+    },
+    {
+      type: 'ROLE',
+      name: 'role',
+      description: 'Roles to limit this emoji to.',
+      required: false,
       choices: undefined,
       options: undefined
     }
   ];
 
-  async invoke(interaction: CommandInteraction, options: Collection<string, any>): Promise<any> {
-    const existingEmoji: string = options.get('emoji');
-    const url = options.get('url');
+  async invoke(
+    interaction: CommandInteraction,
+    {
+      emoji: emojiStr,
+      url,
+      name: { value: name },
+      role
+    }: { emoji: OptionString; url: OptionString; name: OptionString; role: OptionRole }
+  ): Promise<any> {
+    interaction.defer();
 
-    if (existingEmoji) {
-      const res = existingEmoji.match(/\d{17,20}/);
-      console.log(res);
-    } else if (url) {
-      // TODO: create emoji from url
-    } else interaction.reply('No paramaters provided!');
+    let attachment: string | Buffer = '';
+
+    // Create from existing emoji
+    if (emojiStr) {
+      // Get ID from input
+      const [id] = emojiStr.value.match(/\d{17,20}/) ?? [];
+      // If no match, return
+      if (!id) return interaction.reply('Invalid emoji id.', { ephemeral: true });
+      // Create emoji
+      const url = `https://cdn.discordapp.com/emojis/${id}.png?v=1`;
+      const emoji = await interaction
+        .guild!.emojis.create(url, name, {
+          reason: `Requested by ${interaction.user.tag} (${interaction.user.id})`
+        })
+        .catch((e: DiscordAPIError) => e);
+      // If the id was invalid, then return
+      if (emoji instanceof DiscordAPIError) return interaction.editReply('Could not find that emoji!');
+      // Reply with newly created emoji
+      return interaction.editReply(emoji.toString());
+    }
+    // Create from url
+    else if (url) {
+      // TODO: Possible IP Leak, since the url can be pointing to a private server that logs ips. Solutions: use a proxy, or upload the image to discord using url first, then download it from discord.
+      const res = await fetch(url.value)
+        .then(r => r.buffer())
+        .catch((e: FetchError) => e);
+
+      if (res instanceof FetchError) return interaction.editReply('Could not get image.');
+
+      // If emoji smaller than 256kb, then use the original url
+      if (res.byteLength < 256000) attachment = url.value;
+      // Otherwise, resize image
+      else attachment = await sharp(res).toFormat('png').resize(128, 128).toBuffer();
+    }
+    // Neither url or emoji was provided
+    else return interaction.editReply('No paramaters provided!');
+
+    // Create emoji
+    const emoji = await interaction
+      .guild!.emojis.create(attachment, name, {
+        reason: `Requested by ${interaction.user.tag} (${interaction.user.id})`,
+        roles: role ? [role.value] : undefined
+      })
+      .catch((e: DiscordAPIError) => e);
+    if (emoji instanceof DiscordAPIError) interaction.editReply(`Error: ${emoji.message}`);
+    else interaction.editReply(emoji.toString());
   }
 }
